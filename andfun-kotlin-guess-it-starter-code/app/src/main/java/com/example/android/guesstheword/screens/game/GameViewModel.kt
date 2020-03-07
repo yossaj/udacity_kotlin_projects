18bf8c456832ad51b6ba7679259ea106ab8cc276
yossaj <yossaj@icloud.com>
2020-03-07 19:41:22 +0000

    transfered logic and vars from fragment to viewmodel

--- andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -4,12 +4,65 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel(){
+
+    private lateinit var wordList: MutableList<String>
+    var score = 0
+    var word = ""
+
     init{
         Log.i("GameViewModel", "GameViewModel Created ")
+        resetList()
+        nextWord()
     }
 
     override fun onCleared() {
         super.onCleared()
         Log.i("GameViewModel", "GameViewModel Destroyed")
+    }
+
+    private fun resetList() {
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+//            gameFinished()
+        } else {
+            word = wordList.removeAt(0)
+        }
+    }
+
+    fun onSkip() {
+        score--
+        nextWord()
+    }
+
+    fun onCorrect() {
+        score++
+        nextWord()
     }
 }
