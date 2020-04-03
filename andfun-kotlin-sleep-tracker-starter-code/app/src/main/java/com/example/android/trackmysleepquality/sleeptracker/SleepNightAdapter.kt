208372f8bcb27de339e46bc40ea74ae2fccee9a3
yossaj <yossaj@icloud.com>
2020-04-03 19:22:18 +0100

    binding methods abstracted and layout changed from liner to grid

--- andfun-kotlin-sleep-tracker-starter-code/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ andfun-kotlin-sleep-tracker-starter-code/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -1,11 +1,7 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
-import android.content.res.Resources
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -13,6 +9,7 @@ import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.convertDurationToFormatted
 import com.example.android.trackmysleepquality.convertNumericQualityToString
 import com.example.android.trackmysleepquality.database.SleepNight
+import com.example.android.trackmysleepquality.databinding.ListNightItemNightBinding
 
 class SleepNightAdapter :  ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(SleepNightDiffCallback()){
 
@@ -25,38 +22,22 @@ class SleepNightAdapter :  ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>
         return ViewHolder.from(parent)
     }
 
-    class ViewHolder(itemView : View): RecyclerView.ViewHolder(itemView){
-        val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
-        val quality: TextView = itemView.findViewById(R.id.quality_string)
-        val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
+    class ViewHolder(val binding: ListNightItemNightBinding): RecyclerView.ViewHolder(binding.root){
 
+        fun bind(item: SleepNight) {
+            binding.sleep = item
+            binding.executePendingBindings()
 
-       fun bind(item: SleepNight) {
-           val res = itemView.context.resources
-           sleepLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-           quality.text = convertNumericQualityToString(item.sleepQuality, res)
-
-            qualityImage.setImageResource(when (item.sleepQuality) {
-                0 -> R.drawable.ic_sleep_0
-                1 -> R.drawable.ic_sleep_1
-                2 -> R.drawable.ic_sleep_2
-                3 -> R.drawable.ic_sleep_3
-                4 -> R.drawable.ic_sleep_4
-                5 -> R.drawable.ic_sleep_5
-                else -> R.drawable.ic_sleep_active
-            })
         }
 
         companion object {
             fun from(parent : ViewGroup): ViewHolder {
                 val layoutInflator = LayoutInflater.from(parent.context)
-                val view = layoutInflator.inflate(R.layout.list_night_item_night, parent, false)
-                return ViewHolder(view)
+                val binding = ListNightItemNightBinding.inflate(layoutInflator, parent, false )
+                return ViewHolder(binding)
             }
 
         }
-
-
 }
 
     class SleepNightDiffCallback :
--- andfun-kotlin-sleep-tracker-starter-code/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ andfun-kotlin-sleep-tracker-starter-code/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -25,6 +25,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.databinding.FragmentSleepTrackerBinding
@@ -59,8 +60,14 @@ class SleepTrackerFragment : Fragment() {
                 ViewModelProviders.of(this, viewModelFactory).get(SleepTrackerViewModel::class.java)
 
         binding.sleepTrackerViewModel = sleepTrackerViewModel
+
+        val manager = GridLayoutManager(activity, 3)
+        binding.sleepList.layoutManager = manager
+
         val adapter = SleepNightAdapter()
         binding.sleepList.adapter = adapter
+
+
 
         sleepTrackerViewModel.allnights.observe(viewLifecycleOwner, Observer {
             it?.let {
