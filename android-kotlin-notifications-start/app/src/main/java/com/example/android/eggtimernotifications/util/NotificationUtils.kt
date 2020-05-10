7b62e84b210c05035e5655c16009fda66b7f86aa
yossaj <yossaj@icloud.com>
2020-05-10 17:16:26 +0100

    images added to motification and a pending intent to resend notification after 1min

--- android-kotlin-notifications-start/app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ android-kotlin-notifications-start/app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -24,6 +24,8 @@ import android.graphics.BitmapFactory
 import androidx.core.app.NotificationCompat
 import com.example.android.eggtimernotifications.MainActivity
 import com.example.android.eggtimernotifications.R
+import com.example.android.eggtimernotifications.receiver.SnoozeReceiver
+
 //import com.example.android.eggtimernotifications.receiver.SnoozeReceiver
 
 // Notification ID.
@@ -52,8 +54,23 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
     )
 
     // TODO: Step 2.0 add style
+    val eggImage = BitmapFactory.decodeResource(
+        applicationContext.resources,
+        R.drawable.cooked_egg
+    )
+
+    val bigPicStyle = NotificationCompat.BigPictureStyle()
+        .bigPicture(eggImage)
+        .bigLargeIcon(null)
 
     // TODO: Step 2.2 add snooze action
+    val snoozeIntent = Intent(applicationContext, SnoozeReceiver::class.java)
+    val snoozePendingIntent: PendingIntent = PendingIntent.getBroadcast(
+        applicationContext,
+        REQUEST_CODE,
+        snoozeIntent,
+        FLAGS
+    )
 
     // TODO: Step 1.2 get an instance of NotificationCompat.Builder
     // Build the notification
@@ -72,6 +89,13 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
         .setContentText(messageBody)
         .setContentIntent(contentPendingIntent)
         .setAutoCancel(true)
+        .setStyle(bigPicStyle)
+        .setLargeIcon(eggImage)
+        .addAction(
+            R.drawable.egg_icon,
+            applicationContext.getString(R.string.snooze),
+            snoozePendingIntent
+        )
 
 
     // TODO: Step 1.13 set content intent
