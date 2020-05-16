8175a5e7e4eb69d0b9da19f7e1a2d812be39d459
yossaj <yossaj@icloud.com>
2020-05-16 11:20:19 +0100

    added terrain types and a fine old toon as default start point

--- wander/app/src/main/java/com/example/wander/MapsActivity.kt
+++ wander/app/src/main/java/com/example/wander/MapsActivity.kt
@@ -2,6 +2,8 @@ package com.example.wander
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -35,9 +37,38 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val latitude = 55.605488
+        val longitude = -2.460179
+        val zoomLevel = 12f
+        val homeLatLong = LatLng(latitude, longitude)
+
+        map.addMarker(MarkerOptions().position(homeLatLong).title("A fine old Toon"))
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLong, zoomLevel))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.map_options, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem)= when (item.itemId) {
+        R.id.normal_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_NORMAL
+            true
+        }
+        R.id.hybrid_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_HYBRID
+            true
+        }
+        R.id.satellite_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_SATELLITE
+            true
+        }
+        R.id.terrain_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_TERRAIN
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 }
